fe7a6033a89c388935dbb868b70115715e917086
Ben Cressey <bcressey@amazon.com>
2024-01-04 21:56:03 +0000

    go: integrate AWS-LC for FIPS support
    Apply patches to Go to expect AWS-LC instead of BoringCrypto, and
    replace the upstream syso binaries with AWS-LC syso binaries before
    building Go.
    
    Ultimately this means that AWS-LC will be used for Go builds that
    set `GOEXPERIMENT=boringcrypto`.
    
    Signed-off-by: Ben Cressey <bcressey@amazon.com>

---
+++ configs/aws-lc/x86_64-bottlerocket-linux-gnu.toolchain.cmake
@@ -0,0 +1,13 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CMAKE_SYSROOT /x86_64-bottlerocket-linux-gnu/sys-root)
+set(CMAKE_C_COMPILER x86_64-bottlerocket-linux-gnu-gcc)
+set(CMAKE_C_COMPILER_TARGET x86_64-bottlerocket-linux-gnu)
+set(CMAKE_CXX_COMPILER x86_64-bottlerocket-linux-gnu-g++)
+set(CMAKE_CXX_COMPILER_TARGET x86_64-bottlerocket-linux-gnu)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
