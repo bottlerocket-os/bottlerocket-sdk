cc3d3d3e9b06405c9a9b180d37ee21f72713e84e
Jingwei Wang <jweiw@amazon.com>
2026-01-13 00:40:31 +0000

    helpers: delete aws-lc build scripts and prep-go cleanup
    Signed-off-by: Jingwei Wang <jweiw@amazon.com>

--- helpers/go/prep-go.sh
+++ helpers/go/prep-go.sh
@@ -17,22 +17,6 @@ sdk-fetch "${HOME}/hashes-go"
 tar --strip-components=1 -xf go${GOVER}.src.tar.gz
 rm go${GOVER}.src.tar.gz
 
-# Patch Go sources so that they work with AWS-LC as the crypto implementation.
-# Note that this will break use of `GOEXPERIMENT=boringcrypto` when using the
-# default syso files that ship with Go, since the functions and data structures
-# will no longer match. We build the replacement AWS-LC syso files below.
+# Apply Go patches.
 git init
 git apply --whitespace=nowarn "${HOME}"/patches-go/*.patch
-
-# We need to build AWS-LC before we can build Go.
-mkdir -p "${HOME}/aws-lc"
-cd "${HOME}/aws-lc"
-sdk-fetch "${HOME}/hashes-aws-lc"
-AWS_LC_SRC_PACKAGE="aws-lc-AWS-LC-FIPS-${AWS_LC_FIPS_VER}.tar.gz"
-tar --strip-components=1 -xf ${AWS_LC_SRC_PACKAGE}
-rm ${AWS_LC_SRC_PACKAGE}
-
-# Patch AWS-LC sources to avoid weak symbols for memory management functions
-# when GOBORING is defined.
-git init
-git apply --whitespace=nowarn "${HOME}"/patches-aws-lc/*.patch
